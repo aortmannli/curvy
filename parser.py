16521f062a7fa2c8599396508cdda43c2424474b
angel ortmann-li <hyejinkyo@gmail.com>
2020-03-10 18:59:05 -0400

    more

--- parser.py
+++ parser.py
@@ -41,7 +41,7 @@ The file follows the following format:
 
 See the file script for an example of the file format
 """
-ARG_COMMANDS = [ 'line', 'scale', 'move', 'rotate', 'save' ]
+ARG_COMMANDS = [ 'line', 'scale', 'move', 'rotate', 'save', 'circle','bezier','hermite']
 
 def parse_file( fname, edges, transform, screen, color ):
 
@@ -63,7 +63,15 @@ def parse_file( fname, edges, transform, screen, color ):
             add_edge( edges,
                       float(args[0]), float(args[1]), float(args[2]),
                       float(args[3]), float(args[4]), float(args[5]) )
-
+            
+        elif line == 'circle':
+            add_circle(edges,float(args[0]), float(args[1]), float(args[2]),
+                      float(args[3]),0.001)
+        elif line == 'bezier' or line == 'hermite':
+	    add_curve(edges, float(args[0]), float(args[1]), float(args[2]),
+                      float(args[3]), float(args[4]), float(args[5]),
+                      float(args[6]), float(args[7]), 0.001, line)
+	    
         elif line == 'scale':
             #print 'SCALE\t' + str(args)
             t = make_scale(float(args[0]), float(args[1]), float(args[2]))
